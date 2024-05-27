1aab0b4a47d2e6f304afca1d8479a056982aa118
KSH3535 <suhyeon5328@naver.com>
2024-05-27 16:49:48 +0900

    nav에 list 구성

---
+++ React_Exe/ch11_crud/src/ex05_list/SubApp.jsx
@@ -0,0 +1,50 @@
+function Header(props) {
+    return (
+        <header>
+            <h1>
+                <a href='/'>{props.title}</a>
+            </h1>
+        </header>
+    )
+
+}
+
+function Nav() {
+    return (
+        <nav>
+            <ol>
+                <li><a href='/read/1'>html</a></li>
+                <li><a href='/read/2'>css</a></li>
+                <li><a href='/read/3'>js</a></li>
+            </ol>
+        </nav>
+    )
+}
+
+function Article(props) {
+    return (
+        <article>
+            <h2>{props.title}</h2>
+            {props.body}
+        </article>
+    )
+}
+
+
+ export default function SubApp() {
+
+    const topics = [
+        {id:1, title:'html', body:'html is ...'},
+        {id:2, title:'css', body:'css is ...'},
+        {id:3, title:'javascript', body:'javascript is ...'}
+    ]
+    
+    
+    return (
+        <div>
+            <Header title='REACT' />
+            <Nav topics={topics} />
+            <Article title='WELCOME' body='Hello, WEB!' />
+        </div>
+    )
+ }
---
+++ React_Exe/ch11_crud/src/ex08_nav_list/SubApp.jsx
@@ -0,0 +1,57 @@
+function Header(props) {
+    return (
+        <header>
+            <h1>
+                <a href='/'>{props.title}</a>
+            </h1>
+        </header>
+    )
+
+}
+
+function Nav(props) {
+
+    // props로부터 받은 데이터를 <li></li>로 만들어서 lis에 저장
+    const lis = [];
+
+    for (let i = 0; i < props.topics.length; i++) {
+        let t = props.topics[i];
+        lis.push(<li><a href={"/read/" + t.id}>{t.title}</a></li>);
+    }
+
+    return (
+        <nav>
+            <ol>
+                {lis}
+            </ol>
+        </nav>
+    )
+}
+
+function Article(props) {
+    return (
+        <article>
+            <h2>{props.title}</h2>
+            {props.body}
+        </article>
+    )
+}
+
+
+ export default function SubApp() {
+
+    const topics = [
+        {id:1, title:'html', body:'html is ...'},
+        {id:2, title:'css', body:'css is ...'},
+        {id:3, title:'javascript', body:'javascript is ...'}
+    ]
+    
+    
+    return (
+        <div>
+            <Header title='REACT' />
+            <Nav topics={topics} />
+            <Article title='WELCOME' body='Hello, WEB!' />
+        </div>
+    )
+ }
